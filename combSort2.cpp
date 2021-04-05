54f547e2f922a1aaa90f4c58e43076200abff1d5
BaranovAndrey719 <79872174+BaranovAndrey719@users.noreply.github.com>
2021-04-05 13:22:33 +0300

    Добавить combSort.cpp

---
+++ combSort2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+void comb_sort(int* left, int* right) {
+    int size = right - left;
+    if (size <= 1) 
+        return;
+    const double k = 1.2473309;
+    int step = size - 1;
+    while (step > 1) {
+        for (int* l = left; l + step < right; ++l) {
+            if (*l > *(l + step))
+                swap(*l, *(l + step));
+        }
+        step = int(double(step) / k);
+    }
+
+    bool flag = true;
+    while (flag) {
+        flag = false;
+        for (int* l = left; l + 1 < right; ++l) {
+            if (*l > *(l + 1)) {
+                swap(*l, *(l + 1));
+                flag = true;
+            }
+        }
+    }
+
+}
+
+int main() {
+    int N; // количество элементов массива
+    cout << "Введите количество элементов: ";
+    cin >> N; // считываем количество элементов
+    int* array = new int[N];
+    cout << "Введите элементы\n";
+    for (int i = 0; i < N; ++i) {
+        cin >> array[i]; // помещаем считанное число в массив
+    }
+    comb_sort(array, array + N); 
+    cout << "Отсортированный массив: ";
+    for (int i = 0; i < N; ++i) {
+        cout << array[i] << ' ';
+    }
+    delete [] array;
+    return 0;
+}
